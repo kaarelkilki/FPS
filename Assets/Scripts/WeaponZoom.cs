54906e5acde5df44f501a142d2de5b6979aa57f7
kaarelkilki <65207477+kaarelkilki@users.noreply.github.com>
2021-03-31 05:50:25 +0300

    weaponZoom

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomedOutFOW = 60f;
+    [SerializeField] float zoomedInFOW = 20f;
+
+    bool zoomedInToggle = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (zoomedInToggle == false)
+            {
+                zoomedInToggle = true;
+                fpsCamera.fieldOfView = zoomedInFOW;
+            }
+            else if (zoomedInToggle == true)
+            {
+                zoomedInToggle = false;
+                fpsCamera.fieldOfView = zoomedOutFOW;
+            }
+        }
+    }
+}
