0fb861c8a235d61f3bee36d9eb04d1382341bd2b
kaarelkilki <65207477+kaarelkilki@users.noreply.github.com>
2021-04-18 06:22:15 +0300

    Flashlight and battery

---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float ligthDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    public void RestoreLightAngle(float restoreAngle)
+    {
+        myLight.spotAngle = restoreAngle;
+    }
+
+    public void RestoreLightIntensity(float restoreIntensity)
+    {
+        myLight.intensity += restoreIntensity;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (myLight.spotAngle <= minimumAngle)
+        {
+            return;
+        }
+        else
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        if (myLight.intensity <= 0)
+        {
+            return;
+        }
+        else
+        {
+            myLight.intensity -= ligthDecay * Time.deltaTime;
+        }
+    }
+}
